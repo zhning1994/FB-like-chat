f27c631a8b266cbc439fb318f66d5bca07847139
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-21 23:28:43 +0800

    use bcrypth to hash password with 10 salt, then generate user and save it,in the end return user

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-    res.send("hey its auth route");
+router.post("/register", async (req, res) => {
+    try {
+        //create hashed password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        //create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        //save user and respond
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router;
