a1f96a51c5f8970fdeac7bfefc47b831d3e51f93
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-22 23:22:43 +0800

    put method to update user info via  req.body

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,7 +1,26 @@
+const User = require("../models/userModel");
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-    res.send("hey its user route");
-})
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (error) {
+                return res.status(500).json(error);
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
+            res.status(200).json("Account has been updated")
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    } else {
+        return res.status(403).json("You can update only your account!");
+    }
+});
 
 module.exports = router;
