599166f0306869afe67805c3ac8e0509773a29e6
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-24 00:15:09 +0800

    find the post via params.id, if likes property of post include userId then pull the userId from likes array and vice verse

--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -41,8 +41,31 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
-//like a post
+//like or dislike a post
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            res.status(200).json("The post has been liked");
+        } else {
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("The post has been disliked");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 //get a post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 //get timeline posts
 
 module.exports = router;
