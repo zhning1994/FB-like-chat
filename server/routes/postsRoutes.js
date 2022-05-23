a8f123676ae80e54a3e822dd17b2d394a172169a
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-23 19:53:06 +0800

    setup create post route and update post route if post userId is same as request body userId

---
+++ server/routes/postsRoutes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const Post = require("../models/postModel");
+
+router.post("/", async (req, res) => {
+    //create a post
+    const newPost = new Post(req.body)
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+//update a post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("the post has been updated");
+        } else {
+            res.status(403).json("you can update only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//delete a post
+//like a post
+//get a post
+//get timeline posts
+
+module.exports = router;
