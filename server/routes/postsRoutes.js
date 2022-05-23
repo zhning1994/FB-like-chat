af9327cd9fe425e4be6ba3a50927eabe0e50f730
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-23 19:56:42 +0800

    setup delete post route

--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -26,7 +26,21 @@ router.put("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
 //delete a post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne();
+            res.status(200).json("the post has been deleted");
+        } else {
+            res.status(403).json("you can delete only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 //like a post
 //get a post
 //get timeline posts
