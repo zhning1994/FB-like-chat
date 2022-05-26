e2d1664011d17164234e6b24880ade2171bd0743
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-26 22:19:26 +0800

    rightbar will consist of birthday part, advertising part and showing list of friends who are online

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,33 @@
 import "./Rightbar.css";
 
 function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/gift.png" alt="birthday" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img className="rightbarAd" src="/assets/ad.png" alt="ad" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt="profilePic"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Jojo Henry</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Rightbar;
