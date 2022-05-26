596228835b4ea1eda72520b4002dc64a1b582ec8
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-26 18:16:41 +0800

    add Post component into Feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,11 +1,13 @@
 import "./Feed.css";
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
+        <Post />
       </div>
     </div>
   );
