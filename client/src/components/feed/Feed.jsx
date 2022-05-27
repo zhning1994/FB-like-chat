084bbc203fbaca718195101f2b64be924c8a0772
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-27 19:28:52 +0800

    import dummy data Posts and return <Post/> component for each post rendered

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,13 +1,16 @@
 import "./Feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
+import { Posts } from "../../dummyData";
 
 function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        <Post />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
       </div>
     </div>
   );
