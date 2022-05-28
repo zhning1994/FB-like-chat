65184667a44a3e6523ae47bb8f7ed56ca28ebb25
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-28 15:06:22 +0800

    copy the structure of Home page, adding profile picture category on top, for right bottom part of profile, the <Rightbar /> componant will receive profile as props but <Rightbar /> componant on Home page will not receive this prop

---
+++ client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,42 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./Profile.css";
+
+function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt="coverPic"
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt="profilePic"
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Safak Kocaoglu</h4>
+              <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
