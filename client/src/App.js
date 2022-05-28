36a9b6c93c4b16f6a407e6bfff0b5e19f94dc394
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-28 15:12:55 +0800

    import Profile component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from './pages/Home/Home';
+import Profile from './pages/Profile/Profile';
 
 
 function App() {
   return (
-    <Home />
+    <Profile />
   );
 }
 
