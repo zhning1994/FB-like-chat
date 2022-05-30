704cdae9fb8804d1849cc8fa6023320b9e647d97
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-30 14:12:22 +0800

    if having username as prop, then get the posts of that user or else get the posts of current user

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,15 +1,27 @@
 import "./Feed.css";
-import Share from "../share/Share";
 import Post from "../post/Post";
-import { Posts } from "../../dummyData";
+import Share from "../share/Share";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function Feed() {
+function Feed({ username }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("/posts/timeline/" + "628b48cf90e5c63c7e77899f");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [username]);
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
-          <Post key={p.id} post={p} />
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
         ))}
       </div>
     </div>
