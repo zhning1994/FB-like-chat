f8d8fb3fb3be68414596a17a41c0be650421dbc2
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-30 14:15:17 +0800

    when clicking the image of creator of post, go to that user pages

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,12 +1,24 @@
 import "./Post.css";
 import { MoreVert } from "@material-ui/icons";
 import { Users } from "../../dummyData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
 
 function Post({ post }) {
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users/${post.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [post.userId]);
 
   const likeHandler = () => {
     setLike(isLiked ? like - 1 : like + 1);
@@ -17,16 +29,17 @@ function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              className="postProfileImg"
-              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
-              alt="friendsProfilePic"
-            />
+            <Link to={`profile/${user.username}`}>
+              <img
+                className="postProfileImg"
+                src={user.profilePicture || PF + "person/noAvatar.png"}
+                alt="friendsProfilePic"
+              />
+            </Link>
+
             <div className="postUserInfo">
-              <span className="postUsername">
-                {Users.filter((u) => u.id === post.userId)[0].username}
-              </span>
-              <span className="postDate">{post.date}</span>
+              <span className="postUsername">{user.username}</span>
+              <span className="postDate">{format(post.createdAt)}</span>
             </div>
           </div>
           <div className="postTopRight">
@@ -35,7 +48,7 @@ function Post({ post }) {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={PF + post.photo} alt="postImg" />
+          <img className="postImg" src={PF + post.img} alt="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
