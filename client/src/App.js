55d85f0899388b074a61116596f2ac7d16b6fd0a
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-05-31 13:28:00 +0800

    using Navigate for redirec to login and register route if user didn't exist

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,20 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
   Link
 } from "react-router-dom";
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path='/' element={user ? <Home /> : <Register />} />
+        <Route path='/login' element={user ? <Navigate replace to="/" /> : <Login />} />
+        <Route path='/register' element={user ? <Navigate replace to="/" /> : <Register />} />
         <Route path='/profile/:username' element={<Profile />} />
       </Routes>
     </BrowserRouter>
